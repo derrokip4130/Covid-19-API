ff46df95722cde84f8ff508e60502037dd6bb262
Derrick Kiprop <derrickip34@gmail.com>
2024-01-27 19:29:23 +0300

    edit settings file

--- covid_web_api/settings.py
+++ covid_web_api/settings.py
@@ -16,9 +16,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qui5j5js4e(k3#&csz%1e55b03@blvy#skq*#!#lun+gcen5=v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "127.0.0.1 localhost").split(" ")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
