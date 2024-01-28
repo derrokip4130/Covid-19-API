a6fb9bb92a2111e2d0e8a142fbc180e05879253f
Derrick Kiprop <derrickip34@gmail.com>
2024-01-28 16:19:01 +0300

    ensure date is  within specified range

--- webhooks/views.py
+++ webhooks/views.py
@@ -7,6 +7,7 @@ from rest_framework.decorators import action
 from django.db import models
 from django.shortcuts import render
 from datetime import datetime,date
+from django.utils import timezone
 
 def home_page(request):
     title = "COVID 19- Info"
@@ -18,16 +19,35 @@ class ClientSerializer(serializers.ModelSerializer):
         fields = ["date", "state", "tcin", "tcfn", "cured", "death"]
 
 class CaseViewSet(viewsets.ModelViewSet):
+    
     def get_queryset(self):
         fields = self.request.query_params.getlist('field_name', ['death'])
         queries = self.request.query_params.getlist('query', ['gt'])
         values = self.request.query_params.getlist('value', [148500])
+        date_param = self.request.query_params.get('date', None)
 
         filters = {}
 
         for field, query, value in zip(fields, queries, values):
             field_key = f"{field}__{query}"
             filters[field_key] = value
+
+        # Process and validate the date parameter
+        if date_param:
+            try:
+                date_param = timezone.datetime.strptime(date_param, '%Y-%m-%d').date()
+            except ValueError:
+                date_param = None
+
+        # Ensure the date is within the specified range
+        if date_param:
+            start_date = timezone.datetime(2020, 3, 10).date()
+            end_date = timezone.datetime(2023, 4, 29).date()
+
+            date_param = max(start_date, min(date_param, end_date))
+
+            # Add the date filter to the queryset
+            filters['date'] = date_param
 
         queryset = Case.objects.filter(**filters)
 
@@ -78,7 +98,7 @@ class CaseViewSet(viewsets.ModelViewSet):
             'max_cured': max_cured_data['max_cured'],
             'date_of_max_cured': max_cured_data['date_of_max_cured'],
             'min_death': min_death_data['min_death'],
-            'date_of_min_death': min_death_data['min_death'],
+            'date_of_min_death': min_death_data['date_of_min_death'],
             'min_cured': min_cured_data['min_cured'],
             'date_of_min_cured': min_cured_data['date_of_min_cured'],
             # Add more as needed
